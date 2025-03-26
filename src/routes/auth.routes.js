97190299d33f8cc70c3a264756ad247b2033c4ee
rakanugraha18 <rakanugraha2@gmail.com>
2025-03-27 00:00:44 +0700

    Delete something no need from auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -18,30 +18,4 @@ router.get(
   }
 );
 
-// Cek Status Login (Tambahan untuk `/auth/status`)
-router.get("/status", (req, res) => {
-  if (req.isAuthenticated()) {
-    return res.status(200).json({
-      id: req.user.id,
-      firstName: req.user.firstName,
-      lastName: req.user.lastName,
-      email: req.user.email,
-    });
-  } else {
-    return res.status(401).json({ message: "User belum login" });
-  }
-});
-
-// Logout API tanpa redirect
-router.get("/logout", (req, res) => {
-  req.logout((err) => {
-    if (err) return res.status(500).json({ message: "Logout gagal" });
-
-    req.session.destroy(() => {
-      res.clearCookie("connect.sid"); // Hapus session cookie
-      return res.status(200).json({ message: "Logout berhasil" });
-    });
-  });
-});
-
 export default router;
